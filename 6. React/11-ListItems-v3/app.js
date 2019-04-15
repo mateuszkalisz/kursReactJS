7d1dcd9b18caa0506921c09868f2a1fd17648a02
MK <kaliszmat1@gmail.com>
2019-04-15 16:34:21 +0200

    filter members list by sex (male, female and show all)

--- 6. React/11-ListItems-v3/app.js
+++ 6. React/11-ListItems-v3/app.js
@@ -21,7 +21,7 @@ const data = {
   },
 
   {
-    id:3,
+    id:4,
     name: "Stasia",
     age: 20,
     sex: "female"
@@ -41,16 +41,57 @@ const Item = ({user}) => {
   )
 }
 
+
 class ListItems extends React.Component {
 
+state = {
+    usersChoice: 0,
+  }
+
+handleWomenChoice = () =>{
+  this.setState({
+    usersChoice: 1,
+  })
+}
+
+handleMenChoice = () =>{
+  this.setState({
+    usersChoice: 2,
+  })
+}
+
+handleAllChoice = () =>{
+  this.setState({
+    usersChoice: 0,
+  })
+}
+
+
 render(){
+  
   let users = this.props.data.users;
-  users = users.filter(user=>user.sex==="female")
+
+  if(this.state.usersChoice===0){
+    users = users;
+  }
+  else if(this.state.usersChoice===1){
+    users = users.filter(user=>user.sex==="female")
+  }
+  else if(this.state.usersChoice===2){
+    users = users.filter(user=>user.sex==="male")
+  }
+
+
   const items = users.map(user=><Item key={user.id} user={user}/>)
   return(
+    <>
+    <button onClick={this.handleWomenChoice}>Pokaż kobiety</button>
+    <button onClick={this.handleMenChoice}>Pokaż mężczyzn</button>
+    <button onClick={this.handleAllChoice}>Pokaż wszystkich</button>
     <ul>
       {items}
     </ul>
+    </>
   )
 }
 }
