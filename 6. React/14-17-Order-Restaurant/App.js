935d40bce0362e176bd71f5f8228c81ace48538c
MK <kaliszmat1@gmail.com>
2019-04-16 21:35:22 +0200

    create app about menu in restaurant, working on App component and Header component, to get number of orders and price for it

---
+++ 6. React/14-17-Order-Restaurant/App.js
@@ -0,0 +1,20 @@
+class App extends React.Component {
+    state = {
+        items: [
+            {id: 1, name: "herbata", active: true},
+            {id: 2, name: "ziemniaki", active: false},
+            {id: 3, name: "kasza", active: true},
+            {id: 4, name: "zupa", active: true},
+            {id: 5, name: "chleb", active: false},
+            {id: 6, name: "maslo", active: true}
+        ]
+      }
+    render() { 
+        return(
+            <React.Fragment>
+                <Header items={this.state.items}/>
+                <ListItems/>
+            </React.Fragment>
+        );
+    }
+}
