098ae798059714153fc9634dd1a1f198546da3b1
MK <kaliszmat1@gmail.com>
2019-05-28 21:58:30 +0200

    foreign view for done and active tasks, show done date and mark task when is important

--- 8.CreateReactApp/9.ToDoList/todolist/src/components/Task.js
+++ 8.CreateReactApp/9.ToDoList/todolist/src/components/Task.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 const Task = props => {
 
-    const {id, text, date, important, active, finishDate} = props.task;
+    const style = {
+        color: 'red',
+    }
 
-    return (  
-        <div>
-            <strong>{text}</strong> - do <span>{date} </span>
-            <button onClick={()=>props.change(id)}>Zostało zrobione</button><button onClick={()=>props.delete(id)}>X</button>
-        </div>
-    );
+    const {id, text, date, active, important, finishDate} = props.task;
+
+    if(active){
+        return (  
+            <div>
+                <strong style={important ? style : null}>{text}</strong> - do <span>{date} </span>
+                <button onClick={()=>props.change(id)}>Zostało zrobione</button><button onClick={()=>props.delete(id)}>X</button>
+            </div>
+        );
+    }
+    else{
+
+        const finishTime = new Date(finishDate).toLocaleString();
+
+        return(
+            <div>
+                <strong>{text}</strong> <em>(zrobić do {date})</em> 
+                <em><br/>-potwierdzenie wykonania {finishTime}</em><button onClick={()=>props.delete(id)}>X</button>
+            </div>
+        )
+    }
+
 }
  
 export default Task;
