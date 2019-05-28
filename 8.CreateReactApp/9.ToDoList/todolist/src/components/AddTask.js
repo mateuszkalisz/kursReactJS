0702f365337bc3b57e55255e31a659ecdecdeda7
MK <kaliszmat1@gmail.com>
2019-05-28 22:29:35 +0200

    add task form structure

--- 8.CreateReactApp/9.ToDoList/todolist/src/components/AddTask.js
+++ 8.CreateReactApp/9.ToDoList/todolist/src/components/AddTask.js
@@ -1,11 +1,31 @@
 import React,{Component} from 'react';
+import './AddTask.css'
 
 class AddTask extends Component {
-    state = {  }
+    state = { 
+        text: '',
+        checked: false,
+        date: "2019-05-30",
+     }
     render() { 
+
+
         return (
             <div className="form">
-                <div>Dodaj task</div>
+                <div>
+                    <form>
+                        <input placeholder="dodaj zadanie" type="text" value={this.state.text}/> 
+                        
+                        <input id="important" type="checkbox" checked={this.state.checked}/>
+                        <label htmlFor="important">Priorytet</label>
+                            <br/>
+                        <label htmlFor="date">Do kiedy zrobić</label>
+                        <input type="date" value={this.state.date} min="2019-05-28" max="2020-12-31"/>
+                            <br/>
+                        <button className="buttonAddTask">Dodaj</button>
+                        
+                    </form>
+                </div>
             </div>
             
           );
