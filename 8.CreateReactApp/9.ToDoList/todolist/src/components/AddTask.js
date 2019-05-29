041025c39980cf5a46285d7b639f2d9d8fc71492
MK <kaliszmat1@gmail.com>
2019-05-29 18:32:00 +0200

    handle text, checkbox and date, and also set max and min date by date object

--- 8.CreateReactApp/9.ToDoList/todolist/src/components/AddTask.js
+++ 8.CreateReactApp/9.ToDoList/todolist/src/components/AddTask.js
@@ -2,27 +2,55 @@ import React,{Component} from 'react';
 import './AddTask.css'
 
 class AddTask extends Component {
+    minDate = new Date().toISOString().slice(0,10);
     state = { 
         text: '',
         checked: false,
-        date: "2019-05-30",
+        date: this.minDate,
      }
+
+    handleDate = (e) =>{
+        this.setState({
+            date: e.target.value,
+        })
+    }
+
+    handleText = (e) =>{
+        this.setState({
+            text: e.target.value,
+        })
+    }
+
+    handleCheckbox = (e)=>{
+        this.setState({
+            checked: e.target.checked,
+        })
+    }
+
+    handleClick = (e) =>{
+        e.preventDefault();
+        console.log("kliknął");
+    }
+
     render() { 
 
+        let maxDate = this.minDate.slice(0,4) * 1 + 1;
+        
+        maxDate = maxDate + "-12-31";
 
         return (
             <div className="form">
                 <div>
                     <form>
-                        <input placeholder="dodaj zadanie" type="text" value={this.state.text}/> 
+                        <input placeholder="dodaj zadanie" type="text" value={this.state.text} onChange={this.handleText}/> 
                         
-                        <input id="important" type="checkbox" checked={this.state.checked}/>
+                        <input id="important" type="checkbox" checked={this.state.checked} onChange={this.handleCheckbox}/>
                         <label htmlFor="important">Priorytet</label>
                             <br/>
                         <label htmlFor="date">Do kiedy zrobić</label>
-                        <input type="date" value={this.state.date} min="2019-05-28" max="2020-12-31"/>
+                        <input type="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate}/>
                             <br/>
-                        <button className="buttonAddTask">Dodaj</button>
+                        <button onClick={this.handleClick} className="buttonAddTask">Dodaj</button>
                         
                     </form>
                 </div>
