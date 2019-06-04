817442ae6b9bd25142fe1dc4a436e17686b9f89e
MK <kaliszmat1@gmail.com>
2019-06-04 21:39:42 +0200

    react router - how to show component with change page of app without page reload

--- 8.CreateReactApp/10.ReactRouter_SPA/reactrouterapp/src/App.js
+++ 8.CreateReactApp/10.ReactRouter_SPA/reactrouterapp/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Link, } from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+const Home = () =>{
+  return(
+    <h1>Strona startowa</h1>
+  );
+}
+
+const News = () =>{
+  return(
+    <h1>Aktualności</h1>
+  )
+}
+
+const Contact = () =>{
+  return(
+    <h1>Kontakt do nas</h1>
+  )
+}
 
 class App extends React.Component{
   render(){
     return(
-      <BrowserRouter>
+      <Router>
       <div>
       <header>
         <nav>
@@ -14,16 +32,18 @@ class App extends React.Component{
             <li><a href="/news">Aktualności</a></li>
             <li><a href="/contact">Kontakt</a></li> */}
               <li><Link to="/">Start</Link></li>
-              <li><Link to="/contact">Contant</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
               <li><Link to="/news">News</Link></li>
           </ul>
         </nav>
       </header>
       <section>
-        Strona - witaj
+          <Route path="/" exact component={Home}/>
+          <Route path="/news" component={News}/>
+          <Route path="/contact" component={Contact}/>
       </section>
       </div>
-      </BrowserRouter>
+      </Router>
     )
   }
 }
