37e3991759b66d8d9c86e9bce4ad19987a24c974
MK <kaliszmat1@gmail.com>
2019-06-07 22:18:39 +0200

    add basics styles to app

--- 8.CreateReactApp/11.ReactRouter_BasicPage_SPA/myapp/src/layouts/App.js
+++ 8.CreateReactApp/11.ReactRouter_BasicPage_SPA/myapp/src/layouts/App.js
@@ -10,7 +10,7 @@ class App extends Component{
   render(){
     return(
       <Router>
-        <div className="App">
+        <div className="app">
         <header>
           {<Header/>}
         </header>
