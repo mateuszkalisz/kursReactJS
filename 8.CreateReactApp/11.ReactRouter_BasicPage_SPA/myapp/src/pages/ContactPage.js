6fa7944db7560e804cf341a47d8f528343b38082
MK <kaliszmat1@gmail.com>
2019-06-15 22:47:44 +0200

    create form and prompt to safe when change page

--- 8.CreateReactApp/11.ReactRouter_BasicPage_SPA/myapp/src/pages/ContactPage.js
+++ 8.CreateReactApp/11.ReactRouter_BasicPage_SPA/myapp/src/pages/ContactPage.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
+import '../styles/ContactPage.css';
+import {Prompt} from 'react-router-dom';
 
-const ContactPage = () =>{
-    return(
-        <div>ContactPage</div>
-    )
+class ContactPage extends Component{
+
+    state ={
+        value: '',
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        this.setState({
+            value: '',
+        })
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            value: e.target.value,
+        })
+    }
+
+    render(){
+        return(
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea value={this.state.value} onChange={this.handleChange} placeholder="Wpisz wiadomość..."></textarea>
+                    <button >Wyślij</button>
+                </form>
+                <Prompt
+                    when={this.state.value}
+                    message="masz niewypełniony formularz, czy na pewno chcesz opuścić tę stronę?"
+                />
+            </div>
+        )
+    }
 }
 
 export default ContactPage;
