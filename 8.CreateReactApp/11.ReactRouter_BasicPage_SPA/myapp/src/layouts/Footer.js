9738dc0bf52b6b23348b6c6bd7e619a0bdbe7f62
MK <kaliszmat1@gmail.com>
2019-06-16 23:22:22 +0200

    create footer and finish app, use match name which created in component properties to show in footer where we are

--- 8.CreateReactApp/11.ReactRouter_BasicPage_SPA/myapp/src/layouts/Footer.js
+++ 8.CreateReactApp/11.ReactRouter_BasicPage_SPA/myapp/src/layouts/Footer.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return ( 
-        <div>Footer</div>
+        <div className="footer">
+            <h2>Stopka</h2>
+            <Route path="/" exact render={()=>{
+                return(
+                    <p>Jesteś na <span>stronie głównej</span></p>
+                )
+           }}/>
+           <Route path="/:page" exact render={(props)=>{
+                console.log(props);
+                return(
+                    <>
+                    <p>Jesteś na <span>{props.match.params.page}</span></p>
+                    <p>Jesteś na <span>{props.match.url}</span></p>
+                    <p>Jesteś na <span>{props.match.path}</span></p>
+                    </>
+                )
+           }}/>
+           <Route path="/:page/:idProduct" exact render={(props)=>{
+                console.log(props);
+                return(
+                    <>
+                    <p>Jesteś na <span>{props.match.params.idProduct}</span></p>
+                    <p>Jesteś na <span>{props.match.url}</span></p>
+                    <p>Jesteś na <span>{props.match.path}</span></p>
+                    </>
+                )
+           }}/>
+        </div>
      );
 }
  
