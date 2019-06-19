e92277366b9385055e4330c4c6297c3eb713cc54
MK <kaliszmat1@gmail.com>
2019-06-19 20:41:01 +0200

    how to implement sass to react

---
+++ 8.CreateReactApp/12.CSS_w_React/Stylowanie_sass/myapp/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.sass';
+
+const Footer = () =>{
+    return(
+        <footer>
+            <p>stopka</p>
+        </footer>
+    )
+}
+
+export default Footer;
