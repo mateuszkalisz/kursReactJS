026138c9ef70a411b3cbc1bc249f43603bf72bed
MK <kaliszmat1@gmail.com>
2019-07-01 21:50:32 +0200

    add header to repetition app

--- 5.Repetition/17.repetition_of_react_router/myrepetitionapp/src/layouts/Header.js
+++ 5.Repetition/17.repetition_of_react_router/myrepetitionapp/src/layouts/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../styles/Header.css';
+import {Route, Switch} from 'react-router-dom';
+
+import img1 from '../images/header1.jpg';
+import img2 from '../images/header2.jpg';
+import img3 from '../images/header3.jpg';
 
 const Header = () => {
-    return ( <div>
-        header
-    </div>  );
+    return ( 
+        <>
+            <Switch>
+                <Route path='/' exact render={()=> (<img src={img1} alt="city"/>)}/>
+                <Route path='/products' render={()=> (<img src={img2} alt="city"/>)}/>
+                <Route path='/contact' render={()=> (<img src={img3} alt="city"/>)}/>
+                <Route path='/admin' render={()=> (<img src={img1} alt="city"/>)}/>
+                <Route render={()=> (<img src={img1} alt="city"/>)}/>
+            </Switch>
+        </>
+    );
 }
  
 export default Header;
