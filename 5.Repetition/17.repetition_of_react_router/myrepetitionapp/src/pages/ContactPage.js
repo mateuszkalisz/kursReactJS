276deac09391a850a9204c2e965e32c07c3d58b9
MK <kaliszmat1@gmail.com>
2019-07-03 19:57:58 +0200

    repetition about create form with closing protection by prompt component

--- 5.Repetition/17.repetition_of_react_router/myrepetitionapp/src/pages/ContactPage.js
+++ 5.Repetition/17.repetition_of_react_router/myrepetitionapp/src/pages/ContactPage.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React,{Component} from 'react';
+import {Prompt} from 'react-router-dom';
 
-const ContactPage = () => {
-    return (
-        <div>Kontakt</div>
-    );
+import '../styles/ContactPage.css';
+
+class ContactPage extends Component{
+    state={
+        value: '',
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        this.setState({
+            value: '',
+        })
+    }
+    
+    handleChange = (e) =>{
+        this.setState({
+            value: e.target.value,
+        })
+    }
+
+    render(){
+        return(
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea value={this.state.value} onChange={this.handleChange}></textarea>
+                    <button>Wyślij</button>
+                </form>
+                <Prompt
+                when={this.state.value}
+                message="Jesteś w trybie edycji formularza, czy na pewno chcesz opuścić tą stronę?"
+                />
+            </div>
+        )
+    }
 }
+
  
 export default ContactPage;
