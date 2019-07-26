07fa24a579dc0023374e64f98084385530135521
MK <kaliszmat1@gmail.com>
2019-07-26 19:50:55 +0200

    add finish date of task and sign which task is important

--- 5.Repetition/18.repetition_todoapp/mytodoapp/src/components/Task.js
+++ 5.Repetition/18.repetition_todoapp/mytodoapp/src/components/Task.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 const Task = (props) =>{
     
-    const {id,text,date} = props.task;
+    const style = {
+        color: 'red',
+    }
 
-    return(
-        <div>
-            <h4>{text}</h4> <span> - do {date}</span>
-            <button onClick={()=>props.change(id)}>Zostało zrobione</button>
-            <button onClick={()=>props.delete(id)}>X</button>
-        </div>
-    )
+    const {id,text,date,active,important,finishDate} = props.task;
+
+    if(active){
+        return(
+            <div>
+                <h4 style={important ? style : null}>{text}</h4> <span> - do {date}</span>
+                <button onClick={()=>props.change(id)}>Zostało zrobione</button>
+                <button onClick={()=>props.delete(id)}>X</button>
+            </div>
+        )
+    }
+    else{
+        const finish = new Date(finishDate).toLocaleString();
+        return(
+            <div>
+                <h4>{text}</h4> <span> - zrobić do {date} </span>
+                <br/>
+                <p>potwierdzenie wykonania - {finish}</p>
+                <button onClick={()=>props.delete(id)}>X</button>
+            </div>
+        )
+    }
+
+
 }
 
 export default Task;
