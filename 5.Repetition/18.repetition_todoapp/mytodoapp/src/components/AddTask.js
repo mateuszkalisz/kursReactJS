8ad0bc00a398757af44f04686fe552554e937199
MK <kaliszmat1@gmail.com>
2019-07-28 23:47:40 +0200

    create form to add tasks handling in to do app

--- 5.Repetition/18.repetition_todoapp/mytodoapp/src/components/AddTask.js
+++ 5.Repetition/18.repetition_todoapp/mytodoapp/src/components/AddTask.js
@@ -1,14 +1,24 @@
 import React,{Component} from 'react';
+import './AddTask.css'
 
 class AddTask extends Component {
 
     state = {
-
+        text: '',
+        checked: false,
     }
 
     render() { 
         return (
-            <div>Dodaj task</div>
+            <div className="form">
+                <input type="text" value={this.state.text} placeholder="dodaj zadanie"/> 
+                <input type="checkbox" id="important" checked={this.state.checked}/>
+                <label htmlFor="important">Priorytet</label>
+                <br/>
+                <label htmlFor="date">Do kiedy zrobić</label>
+                <input type="date" value="2019-01-25" min="2019-01-01" max="2021-01-01"/>
+                <button>Dodaj</button>
+            </div>
         );
     }
 }
