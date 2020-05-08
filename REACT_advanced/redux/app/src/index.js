baf25a70807de6c177c15cbe4a5738ad7f18d878
MK <kaliszmat1@gmail.com>
2020-05-08 23:46:21 +0200

    middlewares in redux (example - logger) => lesson

--- REACT_advanced/redux/app/src/index.js
+++ REACT_advanced/redux/app/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import rootReducer from './reducers/index';
 import App from './App';
 
-const store = createStore(rootReducer);
+const logger = store => next => action =>{
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action); // nie mozemy zgubic wywolania tej akcji wiec trzeba wywolac kolejne middleware
+
+  console.log('next state', store.getState());
+  console.groupEnd();
+
+  return result;
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
